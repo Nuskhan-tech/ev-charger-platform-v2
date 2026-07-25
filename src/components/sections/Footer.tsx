4a09feffcef01e4d4d38ae4ab04ce8a4242cbf73
Nuskhan-tech <153717679+Nuskhan-tech@users.noreply.github.com>
2026-07-25 14:16:39 +0400

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -16,7 +16,7 @@ const solutionLinks = [
 ];
 
 const socials = [
-  { icon: Linkedin, href: 'https://www.linkedin.com/company/futureway-technical-services-llc/posts/?feedView=all', label: 'LinkedIn' },
+  { icon: Linkedin, href: 'https://www.linkedin.com/company/futureway-technical-services-llc/posts/?feedView=all', label: 'LinkedIn',target: '_blank' },
   { icon: Twitter, href: '#', label: 'Twitter' },
   { icon: Facebook, href: '#', label: 'Facebook' },
   { icon: Instagram, href: '#', label: 'Instagram' },
